e81bd98d96d3b3022458a46aed348044567fef54
Smith Samuel <smithaitufe@live.com>
2021-03-13 14:58:16 +0100

    default installation

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+export default {
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/"],
+};
